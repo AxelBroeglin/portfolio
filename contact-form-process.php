1d40c281b60b34d701415ae502e2e81f0ba7cd0e
Axel Broeglin <axelbroeglin@gmail.com>
2022-06-02 18:10:24 +0100

    Created first name input

--- contact-form-process.php
+++ contact-form-process.php
@@ -16,14 +16,16 @@ if (isset($_POST['Email'])) {
 
     // validation expected data exists
     if (
-        !isset($_POST['Name']) ||
+        !isset($_POST['Firstname']) ||
+        !isset($_POST['Lastname']) ||
         !isset($_POST['Email']) ||
         !isset($_POST['Message'])
     ) {
         problem('We are sorry, but there appears to be a problem with the form you submitted.');
     }
 
-    $name = $_POST['Name']; // required
+    $firstname = $_POST['Firstname']; // required
+    $name = $_POST['Lastname']; // required
     $email = $_POST['Email']; // required
     $message = $_POST['Message']; // required
 
@@ -36,8 +38,12 @@ if (isset($_POST['Email'])) {
 
     $string_exp = "/^[A-Za-z .'-]+$/";
 
+    if (!preg_match($string_exp, $firstname)) {
+        $error_message .= 'The First name you entered does not appear to be valid.<br>';
+    }
+
     if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+        $error_message .= 'The Last name you entered does not appear to be valid.<br>';
     }
 
     if (strlen($message) < 2) {
@@ -56,7 +62,8 @@ if (isset($_POST['Email'])) {
         return str_replace($bad, "", $string);
     }
 
-    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Firstname: " . clean_string($firstname) . "\n";
+    $email_message .= "Lastname: " . clean_string($name) . "\n";
     $email_message .= "Email: " . clean_string($email) . "\n";
     $email_message .= "Message: " . clean_string($message) . "\n";
 
@@ -69,7 +76,7 @@ if (isset($_POST['Email'])) {
 
     <!-- include your success message below -->
 
-    Thank you for contacting us. We will be in touch with you very soon.
+    Thank you for contacting me ! I will be in touch with you very soon.
 
 <?php
 }
