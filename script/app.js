01903ac005b46c05fdbaf6e210c6397b7ae50485
Axel Broeglin <axelbroeglin@gmail.com>
2022-06-02 23:11:15 +0100

    Menu is top

--- script/app.js
+++ script/app.js
@@ -28,24 +28,3 @@ menuBtn.addEventListener('click', () => {
         navOpen = false;
     }
 });
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar and main
-const navbar = document.getElementById('navbar');
-const stickyMain = document.getElementById('main-homepage');
-// Get the offset position of the navbar
-const sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    stickyMain.classList.add('stick-main');
-  } else {
-    navbar.classList.remove("sticky");
-    stickyMain.classList.remove('stick-main');
-  }
-}
-//clic sur menu doit changer la couleur de la nav. prendre en compte que nav et body ont 4vh de décalage quand nav va prendre 100vh
